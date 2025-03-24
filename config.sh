41a19205e93bcb6be49ea1099e201c7779761370
Sergey Vorontsov <aurum1997@gmail.com>
2025-03-24 13:26:21 +0300

    Remove gamescope-sdl-workaround from installer

--- config.sh
+++ config.sh
@@ -20,7 +20,6 @@ install_base()
   sudo cp -f gamescope-session/usr/bin/export-gpu /usr/bin/export-gpu
   sudo cp -f gamescope-session/usr/bin/gamescope-session-plus /usr/bin/gamescope-session-plus
   sudo cp -f gamescope-session/usr/lib/systemd/user/gamescope-session-plus@.service /usr/lib/systemd/user/gamescope-session-plus@.service
-  sudo cp -f gamescope-session/usr/libexec/gamescope-sdl-workaround /usr/libexec/gamescope-sdl-workaround
   sudo mkdir -p /usr/share/gamescope-session-plus 
   sudo cp -f gamescope-session/usr/share/gamescope-session-plus/device-quirks /usr/share/gamescope-session-plus/device-quirks
   sudo cp -f gamescope-session/usr/share/gamescope-session-plus/gamescope-session-plus /usr/share/gamescope-session-plus/gamescope-session-plus 
